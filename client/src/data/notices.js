2571ecaddff3c43524257aec5011aa58fb67e10a
warmachine028 <warmachine028@outlook.com>
2022-08-27 00:33:04 +0530

    Modified and Added Mock Data Collections

---
+++ client/src/data/notices.js
@@ -0,0 +1,9 @@
+const notices = [
+	"All face-to-face classes for students of all years will remain suspended from 16th March 2020 till 31st March 2020. All the students and stakeholders are requested to follow our website ( www.uem.edu.in) for any further notifications.",
+	"Though face-to-face classes are suspended, the College/University will be delivering all teaching remotely from 16th March 2020 till 31st March 2020. College/University will be delivering all formal written examinations remotely or where it is not possible to deliver exams remotely they will be rescheduled. Online teaching methods can be accessed through www.uem.edu.in / www.iemcrp.com / www.iemlearning.com",
+	"Hostels will remain open and operative. We advise students and their guardians not to encourage movement of the students out of hostels or travelling by train or by air. The foreign students are advised not to try to go to their home country as they may find acute problems in the airport, railway stations or at the border. In case they are quarantined, college/university authorities will be completely helpless in helping them out. Hence it is strongly advised that students should not move out.",
+	"Also staying in hostels will help the students to access teaching resources, computing & lab resources, library, licensed software, high speed internet, etc.",
+	"All teaching resources like computing & lab resources, library, licensed software, high speed internet, etc. will be available, but no mass gathering are encouraged.",
+]
+
+export default notices
