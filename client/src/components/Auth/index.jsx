54c2dd00ab18809b530a9b0461f2b70ae8f61d3c
warmachine028 <warmachine028@outlook.com>
2022-08-27 20:21:20 +0530

    Added fixes in Issue Templates

--- client/src/components/Auth/index.jsx
+++ client/src/components/Auth/index.jsx
@@ -14,11 +14,11 @@ import {
 	FormControl,
 	Checkbox,
 } from "@mui/material"
-import { styled } from "@mui/material/styles"
+
 import { Visibility, VisibilityOff, LockOutlined } from "@mui/icons-material"
 import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react"
-import { Root, classes } from "./styles"
+import { Root, classes, BpCheckedIcon, BpIcon } from "./styles"
 
 // * Dynamic Objects
 // * These objects are to be fetched OR created dynamically
@@ -112,13 +112,27 @@ const Auth = ({ setUser }) => {
 							<InputLabel id="role-label" style={{ top: 5 }}>
 								Role
 							</InputLabel>
-							<Select fullWidth label="Role" labelId="role-label" value={formData.role} onChange={handleChange} name='role'>
-								<MenuItem name='role' value="teacher">Staff & Faculty</MenuItem>
-								<MenuItem name='role' value="student">Student</MenuItem>
+							<Select fullWidth label="Role" labelId="role-label" value={formData.role} onChange={handleChange} name="role">
+								<MenuItem name="role" value="teacher">
+									Staff & Faculty
+								</MenuItem>
+								<MenuItem name="role" value="student">
+									Student
+								</MenuItem>
 							</Select>
 						</FormControl>
 
-						<Button type="submit" fullWidth className={classes.submit} style={{ color: "white" }}>
+						<Button
+							type="submit"
+							fullWidth
+							className={classes.submit}
+							sx={{
+								// "&:hover": { backgroundColor: "#564f95" },
+							}}
+							style={{
+								color: "white",
+							}}
+						>
 							SIGN IN
 						</Button>
 						<Grid container justifyContent="space-between">
@@ -136,43 +150,6 @@ const Auth = ({ setUser }) => {
 		</Root>
 	)
 }
-const BpIcon = styled("span")(({ theme }) => ({
-	borderRadius: 3,
-	width: 16,
-	height: 16,
-	boxShadow: theme.palette.mode === "dark" ? "0 0 0 1px rgb(16 22 26 / 40%)" : "inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)",
-	backgroundColor: theme.palette.mode === "dark" ? "#394b59" : "#f5f8fa",
-	backgroundImage: theme.palette.mode === "dark" ? "linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))" : "linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
-	".Mui-focusVisible &": {
-		outline: "2px auto black",
-		outlineOffset: 2,
-	},
-	"input:hover ~ &": {
-		backgroundColor: theme.palette.mode === "dark" ? "#30404d" : "#ebf1f5",
-	},
-	"input:disabled ~ &": {
-		boxShadow: "none",
-		background: theme.palette.mode === "dark" ? "rgba(57,75,89,.5)" : "rgba(206,217,224,.5)",
-	},
-}))
-
-const BpCheckedIcon = styled(BpIcon)({
-	backgroundColor: "black",
-	backgroundImage: "linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))",
-	"&:before": {
-		display: "block",
-		width: 16,
-		height: 16,
-		backgroundImage:
-			"url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
-			" fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
-			"1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
-		content: '""',
-	},
-	"input:hover ~ &": {
-		backgroundColor: "gray",
-	},
-})
 
 const BpCheckbox = props => {
 	return (
