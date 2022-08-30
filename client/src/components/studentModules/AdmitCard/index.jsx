e75f9ede624be2b24c6e4b79e069c5960c84035d
warmachine028 <warmachine028@outlook.com>
2022-08-30 21:06:10 +0530

    Added fixes for Admit Cards 📇🃏 #30

--- client/src/components/studentModules/AdmitCard/index.jsx
+++ client/src/components/studentModules/AdmitCard/index.jsx
@@ -1,6 +1,42 @@
+import { Root, classes } from "./styles"
+import { Tabs, Typography, Tab, Box, Button } from "@mui/material"
+import SwipeableViews from "react-swipeable-views-react-18-fix"
+import { useTheme } from "@mui/material/styles"
+import { useState } from "react"
+import Card from "./Card"
+import subjects from "../../../data/subjects.js"
 
-const AdmitCard = () => {
-	return <div>AdmitCard</div>
+const TabPanel = ({ children, value, index, ...other }) => (
+	<div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+		{value === index && <Box>{children}</Box>}
+	</div>
+)
+
+const AdmitCard = ({ user }) => {
+	const theme = useTheme()
+	const [value, setValue] = useState(0)
+	const currentSemester = user.semester - 1
+	const semesters = [...Array(currentSemester).keys()]
+	return (
+		<Root className={classes.root}>
+			<Typography className={classes.title} children="admit cards" />
+			<div className={classes.mainContainer}>
+				<Tabs value={value} onChange={(_, newValue) => setValue(newValue)} aria-label="basic tabs">
+					{semesters.map(sem => (
+						<Tab label={`SEMESTER ${sem + 1}`} key={sem} />
+					))}
+				</Tabs>
+				<SwipeableViews axis={theme.direction === "rtl" ? "x-reverse" : "x"} index={value} onChangeIndex={index => setValue(index)}>
+					{semesters.map(sem => (
+						<TabPanel value={value} index={sem} dir={theme.direction} key={sem}>
+							<Card user={user} sem={sem + 1} subjects={subjects[sem + 1] || subjects[1]} />
+						</TabPanel>
+					))}
+				</SwipeableViews>
+			</div>
+			<Button variant="contained" children="DOWNLOAD" />
+		</Root>
+	)
 }
 
 export default AdmitCard
---
+++ client/src/components/studentModules/AdmitCard/styles.js
@@ -0,0 +1,32 @@
+import { styled } from "@mui/material/styles"
+
+const PREFIX = "AdmitCard"
+export const classes = {
+	root: `${PREFIX}-root`,
+	title: `${PREFIX}-title`,
+	mainContainer: `${PREFIX}-mainContainer`,
+}
+
+export const Root = styled("div")(({ theme }) => ({
+	[`&.${classes.root}`]: {
+		width: "auto",
+		margin: 20,
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+	[`& .${classes.title}`]: {
+		textAlign: "center",
+		textTransform: "uppercase",
+		fontWeight: 500,
+		fontSize: 22,
+		textDecoration: "underline",
+	},
+	[`& .${classes.mainContainer}`]: {
+		display: "flex",
+		flexDirection: "column",
+        alignItems: "center",
+        // overflow: 'scroll',
+        width: '100%'
+	},
+}))
