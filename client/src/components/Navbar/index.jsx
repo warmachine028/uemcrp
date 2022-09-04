9860757ddcbe1a17bad82cf1695e55bfba7f95a1
warmachine028 <warmachine028@outlook.com>
2022-09-04 08:42:23 +0530

    Added minor tweaks

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import { AppBar, Button, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
-import logo from "../../assets/logo.png"
+import logo from "../../images/logo.png"
 import { classes, Root } from "./styles"
 
 const Navbar = ({ user, setUser }) => {
