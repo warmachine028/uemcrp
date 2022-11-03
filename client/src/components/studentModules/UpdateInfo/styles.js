4a42049c435d574ddea3b9b5c89fd8f23330f1b8
Anuj Upadhyaya <anujupadhyaya8@gmail.com>
2022-11-03 13:25:53 +0530

    Formatted styles file

--- client/src/components/studentModules/UpdateInfo/styles.js
+++ client/src/components/studentModules/UpdateInfo/styles.js
@@ -3,22 +3,22 @@ import { styled } from "@mui/material/styles"
 const PREFIX = "UpdateInfo"
 
 export const classes = {
-	root: `${PREFIX}-root`,
+    root: `${PREFIX}-root`,
     input: `${PREFIX}-input`,
-	submitButton: `${PREFIX}-submitButton`,
+    submitButton: `${PREFIX}-submitButton`,
 }
 
 export const Root = styled("div")(({ theme }) => ({
-	[`&.${classes.root}`]: {
-		margin: 32,
-	},
-	[`& .${classes.input}`]: {
-		paddingBottom: 16,
-	},
+    [`&.${classes.root}`]: {
+        margin: 32,
+    },
+    [`& .${classes.input}`]: {
+        paddingBottom: 16,
+    },
     [`& .${classes.submitButton}`]: {
-		margin: "10px 0",
-		color: "white",
-		backgroundColor: "#393464",
-		"&:hover": { backgroundColor: "#564f95" },
-	},
+        margin: "10px 0",
+        color: "white",
+        backgroundColor: "#393464",
+        "&:hover": { backgroundColor: "#564f95" },
+    },
 }))
